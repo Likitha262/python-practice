f5807c7912c2f5f8e332195da6bfc073b109a3f1
G.Likitha <likithagunna95@gmail.com>
2026-09-22 13:17:47 +0530

    Create age_calculator.py

---
+++ Day01_Variables/age_calculator.py
@@ -0,0 +1,8 @@
+birth_year=int(input("enter the birth year:"))
+current_year=int(input("enter the current year:"))
+current_age=current_year-birth_year
+print("my age is",current_age,"years")
+#sample output
+#enter the birth year = 2007
+#enter the current year = 2026
+#my age is 19 years
