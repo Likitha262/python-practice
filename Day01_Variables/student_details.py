dd0fd0c59b650d39fc8394f2a9fac42777e68347
G.Likitha <likithagunna95@gmail.com>
2026-09-22 18:41:28 +0530

    Create student_details.py

---
+++ Day01_Variables/student_details.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+branch = (input("Enter your branch:")
+college=(input("Enter your college:")
+print("student details")
+print("My name is",name)
+print("I am studying in",branch)
+print("I am studying in",college,"college")
+
+
